0758d71634b3375318a500e4a0eddab9b45217a6
Jon Lighthall <jon.lighthall@gmail.com>
2019-10-04 17:58:50 -0500

    remove manual grid input
    continue devel from Mar 16, 2019
    remove input arguments
    remove notes on input and control
    update nargin handling
    remove N_FIG calc
    simplify sorting

--- autoArrangeFigures.m
+++ autoArrangeFigures.m
@@ -1,7 +1,5 @@
-function autoArrangeFigures(NH, NW, monitor_id)
+function autoArrangeFigures(monitor_id)
 % INPUT  :
-%        NH : number of grid of vertical direction
-%        NW : number of grid of horizontal direction
 % OUTPUT :
 %
 % get every figures that are opened now and arrange them.
@@ -10,37 +8,21 @@ function autoArrangeFigures(NH, NW, monitor_id)
 % If you are dual(or more than that) monitor user, I recommend to set wide
 % monitor as Monitor1.
 %
-% if you want arrange automatically, type 'autoArrangeFigures(0,0)' or 'autoArrangeFigures()'. 
-%    But maximum number of figures for automatic mode is 27.
-%
-% if you want specify grid for figures, give numbers for parameters.
-%    but if your grid size is smaller than required one for accommodating
-%    all figures, this function changes to automatic mode and if more
-%    figures are opend than maximum number, then it gives error.
-%
 % Notes
 %  + 2017.1.20 use monitor id(Adam Danz's idea)
 %
 % leejaejun, Koreatech, Korea Republic, 2014.12.13
 % jaejun0201@gmail.com
 
-if nargin < 2
-    NH = 0;
-    NW = 0;
+if nargin < 1
     monitor_id = 1;
 end
 
 task_bar_offset = [0 20];
 
 %%
-N_FIG = NH * NW;
-if N_FIG == 0
-    autoArrange = 1;
-else
-    autoArrange = 0;
-end
-figHandle = sortFigureHandles(findobj('Type','figure'));
-n_fig = size(figHandle,1);
+figHandle = findobj('Type','figure');
+n_fig = length(figHandle);
 if n_fig <= 0
     warning('figures are not found');
     return
