5d5cfbe23e2a84c93c6f9236180ab9c0f96af1fa
Jon Lighthall <jon.lighthall@gmail.com>
2019-10-29 13:22:10 -0500

    add combinatorics

---
+++ comb.m
@@ -0,0 +1,14 @@
+function [outputArg1,A] = comb(inputArg1,inputArg2)
+%COMB Calculates the combinations of inputArg1 choose inputArg2
+% See also perm
+outputArg1 = factorial(inputArg1)/(factorial(inputArg2)*factorial(inputArg1-inputArg2));
+k=1;
+A=zeros(outputArg1,2);
+for i=1:inputArg1-1
+    for j=i+1:inputArg1
+        A(k,:)=[i,j];
+        k=k+1;
+    end
+end
+end
+
---
+++ perm.m
@@ -0,0 +1,19 @@
+function [outputArg1,A] = perm(inputArg1,inputArg2)
+%PERM Calculates the permutations of inputArg1 choose inputArg2
+% See also COMB
+outputArg1 = factorial(inputArg1)/factorial(inputArg1-inputArg2);
+k=1;
+A=zeros(outputArg1,2);
+for i=1:inputArg1
+    for j=1:inputArg1
+        if(i==j)
+            continue
+        else
+            
+            A(k,:)=[i,j];
+            k=k+1;
+        end;
+    end
+end
+end
+
