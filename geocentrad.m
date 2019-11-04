51612b281ac78d7d290aaa5bd39297b28e5e79a6
Jon Lighthall <jon.lighthall@gmail.com>
2019-11-04 15:29:50 -0600

    expand description

--- geocentrad.m
+++ geocentrad.m
@@ -2,17 +2,19 @@ function rad = geocentrad(lat,req,rpol)
 %GEOCENTRAD Calcuates geocentric radius as a function of geodetic latitude.
 %   Returns the distance RAD from the center of a spheroid to an elipical
 %   surface with equatorial radius REQ and polar radius RPOL at geodetic
-%   latitude LAT.
+%   latitude LAT, specified in degrees.
 %
 %   RAD = GEOCENTRAD(LAT) returns geocentric radius of the earth in meters
-%   at latidue LAT.
+%   at latidue LAT in degrees.
 %
 %   RAD = GEOCENTRAD(LAT,REQ,RPOL) returns the radius of an ellipsoid with
-%   semimajor (equitorial) axis REQ and semiminor (polar) axis RPOL.
+%   semimajor (equitorial) axis REQ and semiminor (polar) axis RPOL at
+%   latitude LAT in degrees. The units of RAD will match the units of REQ
+%   and RPOL.
 
 switch nargin
     case 3
-        
+        fprintf('calculating radius for ellipsoid with semimajor axis %f and semiminor axis %f',req,rpol);
     case 1
         E=wgs84Ellipsoid;
         req=E.SemimajorAxis;
@@ -28,4 +30,3 @@ phi=deg2rad(lat);
 
 rad = sqrt(((a^2*cos(phi))^2+(b^2*sin(phi))^2)/((a*cos(phi))^2+(b*sin(phi))^2));
 end
-
