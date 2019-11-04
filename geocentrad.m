2476fe6fd5dba19d36ce39785da4a3cfdb7b82a0
Jon Lighthall <jon.lighthall@gmail.com>
2019-11-04 16:01:31 -0600

    change to semimajor and flattening to match geocradius

--- geocentrad.m
+++ geocentrad.m
@@ -1,24 +1,27 @@
-function rad = geocentrad(lat,req,rpol)
+function rad = geocentrad(lat,f,req)
 %GEOCENTRAD Calcuates geocentric radius as a function of geodetic latitude.
 %   Returns the distance RAD from the center of a spheroid to an elipical
-%   surface with equatorial radius REQ and polar radius RPOL at geodetic
+%   surface with flattening F and equatorial radius REQ at geodetic
 %   latitude LAT, specified in degrees.
 %
 %   RAD = GEOCENTRAD(LAT) returns geocentric radius of the earth in meters
 %   at latidue LAT in degrees.
 %
-%   RAD = GEOCENTRAD(LAT,REQ,RPOL) returns the radius of an ellipsoid with
-%   semimajor (equitorial) axis REQ and semiminor (polar) axis RPOL at
-%   latitude LAT in degrees. The units of RAD will match the units of REQ
-%   and RPOL.
+%   RAD = GEOCENTRAD(LAT,F,REQ) returns the radius of an ellipsoid with
+%   flattening F and semimajor (equitorial) axis REQ at latitude LAT in
+%   degrees. The units of RAD will match the units of REQ.
+% 
+%   Replaces GEOCRADIUS
 
 switch nargin
     case 3
+        rpol=req*(1-f);
         fprintf('calculating radius for ellipsoid with semimajor axis %f and semiminor axis %f',req,rpol);
     case 1
         E=wgs84Ellipsoid;
         req=E.SemimajorAxis;
-        rpol=E.SemiminorAxis;
+        f=E.Flattening;
+        rpol=req*(1-f);
     otherwise
         disp('please enter either 1 or 3 arguments')
         return
@@ -28,5 +31,6 @@ a=req;
 b=rpol;
 phi=deg2rad(lat);
 
+% from https://en.wikipedia.org/wiki/Earth_radius#Geocentric_radius
 rad = sqrt(((a.^2.*cos(phi)).^2+(b.^2.*sin(phi)).^2)./((a.*cos(phi)).^2+(b.*sin(phi)).^2));
 end
