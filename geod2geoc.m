5e3cc23e08cb40917b893ed61a5cc8f374a4e4c6
Jon Lighthall <jon.lighthall@gmail.com>
2019-11-06 14:03:12 -0600

    match outputs of both toolbox functions!

--- geod2geoc.m
+++ geod2geoc.m
@@ -1,4 +1,4 @@
-function [glat] = geod2geoc(lat,f,do_match)
+function [glat] = geod2geoc(lat,f,do_match,~)
 %GEOD2GEOC Convert geodetic latitude to geocentric latitude.
 %   Replaces geodeticLatitudeFromGeocentric from the Mapping Toolbox and
 %   geod2geoc from the Aerospace Toolbox.
@@ -6,19 +6,33 @@ function [glat] = geod2geoc(lat,f,do_match)
 %   See also GEOCENTRICLATITUDE GEODETICLATITUDEFROMGEOCENTRIC GEOC2GEOD
 do_rad=false;
 switch nargin
+    case 4
+        % assuming geod2geoc input
+        lat=lat';
+        f=do_match;
+        do_match=true;
     case 3
-        if(strcmp(do_match,'degrees')==1)
-            do_match=true;
-        end
-        if(strcmp(do_match,'radians')==1)
-            do_match=true;
-            do_rad=true;
+        if(ischar(do_match))
+            % assuming geodeticLatitudeFromGeocentric input
+            if(strcmp(do_match,'degrees')==1)
+                do_match=true;
+            elseif(strcmp(do_match,'radians')==1)
+                do_match=true;
+                do_rad=true;
+            else
+                % assuming geod2geoc input
+                lat=lat';
+                E=referenceEllipsoid(do_match);
+                f=E.Flattening;
+            end
         end
     case 2
-        do_match=false;
-    otherwise
-        disp('please')
-        return
+        if f>1/5
+            % assuming geod2geoc input
+            E=referenceEllipsoid('Earth');
+            f=E.Flattening;
+        end
+        do_match=true;
 end
 
 if(f==0)
