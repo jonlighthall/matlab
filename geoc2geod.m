53482a5d1075a2cbdcb87a2843f4a9e6ee1defaf
Jon Lighthall <jon.lighthall@gmail.com>
2019-11-06 12:49:20 -0600

    add descriptions and f=0 case

--- geoc2geod.m
+++ geoc2geod.m
@@ -1,6 +1,9 @@
 function [glat] = geoc2geod(lat,f,do_match)
-%UNTITLED7 Summary of this function goes here
-%   Detailed explanation goes here
+%GEOC2GEOD Convert geocentric latitude to geodetic latitude.
+%   Replaces geocentricLatitude from the Mapping Toolbox and geoc2geod from
+%   the Aerospace Toolbox.
+%
+%   See also GEODETICLATITUDEFROMGEOCENTRIC GEOCENTRICLATITUDE GEOD2GEOC
 switch nargin
     case 3
     case 2
@@ -17,7 +20,9 @@ q2=((lat> 90)&(lat<=180));
 q3=((lat>180)&(lat<=270));
 q4=((lat>270)&(lat<=360));
 
-if(do_match)
+if(f~=0 && do_match)
+    % match the output of geodeticLatitudeFromGeocentric using atan2, which
+    % maps to -180 to +180
     glat=glat+q2*180+q3*-180+q4*0;
 else
     glat=glat+q2*180+q3*180+q4*360;
--- geod2geoc.m
+++ geod2geoc.m
@@ -1,6 +1,9 @@
-function [glat] = geodc2geoc(lat,f,do_match)
-%UNTITLED7 Summary of this function goes here
-%   Detailed explanation goes here
+function [glat] = geod2geoc(lat,f,do_match)
+%GEOD2GEOC Convert geodetic latitude to geocentric latitude.
+%   Replaces geodeticLatitudeFromGeocentric from the Mapping Toolbox and
+%   geod2geoc from the Aerospace Toolbox.
+%
+%   See also GEOCENTRICLATITUDE GEODETICLATITUDEFROMGEOCENTRIC GEOC2GEOD 
 switch nargin
     case 3
     case 2
@@ -17,7 +20,9 @@ q2=((lat> 90)&(lat<=180));
 q3=((lat>180)&(lat<=270));
 q4=((lat>270)&(lat<=360));
 
-if(do_match)
+if(f~=0 & do_match)
+    % match the output of geodeticLatitudeFromGeocentric using atan2, which
+    % maps to -180 to +180
     glat=glat+q2*180+q3*-180+q4*0;
 else
     glat=glat+q2*180+q3*180+q4*360;
