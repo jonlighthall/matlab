f17d0c1bc11c63cbc1808d03a32a603ec5a2cbc4
Jon Lighthall <jon.lighthall@gmail.com>
2019-11-06 09:49:00 -0600

    rename files and add geod2geoc

---
+++ geod2geoc.m
@@ -0,0 +1,26 @@
+function [glat] = geodc2geoc(lat,f,do_match)
+%UNTITLED7 Summary of this function goes here
+%   Detailed explanation goes here
+switch nargin
+    case 3
+    case 2
+        do_match=false;
+    otherwise
+        disp('please')
+        return
+end
+
+glat=atan(tan(deg2rad(lat))*((1-f)^2));
+
+glat=rad2deg(glat);
+q2=((lat> 90)&(lat<=180));
+q3=((lat>180)&(lat<=270));
+q4=((lat>270)&(lat<=360));
+
+if(do_match)
+    glat=glat+q2*180+q3*-180+q4*0;
+else
+    glat=glat+q2*180+q3*180+q4*360;
+end
+end
+
