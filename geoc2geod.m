86f93326693bb9bf5207737a7d2cac23e7aec54b
Jon Lighthall <jon.lighthall@gmail.com>
2019-11-07 08:54:30 -0600

    add handling for geoc2geod input

--- geoc2geod.m
+++ geoc2geod.m
@@ -1,24 +1,40 @@
-function [glat] = geoc2geod(lat,f,do_match)
-%GEOC2GEOD Convert geocentric latitude to geodetic latitude.
+function [glat] = geoc2geod(lat,f,do_match,~)
+%GEOC2GEOD Convert geocentric latitude to geodetic latitude. Only valid for
+%points on the surface of the ellipsoid.
 %   Replaces geocentricLatitude from the Mapping Toolbox and geoc2geod from
 %   the Aerospace Toolbox.
 %
 %   See also GEODETICLATITUDEFROMGEOCENTRIC GEOCENTRICLATITUDE GEOD2GEOC
 do_rad=false;
 switch nargin
+    case 4
+        % assuming geod2geoc input
+        lat=lat';
+        f=do_match;
+        do_match=true;
     case 3
-        if(strcmp(do_match,'degrees')==1)
-            do_match=true;
-        end
-        if(strcmp(do_match,'radians')==1)
-            do_match=true;
-            do_rad=true;
+        if(ischar(do_match))
+            % assuming geocentricLatitude input
+            if(strcmp(do_match,'degrees')==1)
+                do_match=true;
+            elseif(strcmp(do_match,'radians')==1)
+                do_match=true;
+                do_rad=true;
+            else
+                % assuming geoc2geod input
+                E=referenceEllipsoid(do_match);
+                f=E.Flattening;
+                fprintf('not valid for non-zero altitudes')
+            end
         end
     case 2
+        if f>1/5
+            % assuming geod2geoc input
+            E=referenceEllipsoid('Earth');
+            f=E.Flattening;
+            fprintf('not valid for non-zero altitudes')
+        end
         do_match=true;
-    otherwise
-        disp('please')
-        return
 end
 
 if(f==0)
