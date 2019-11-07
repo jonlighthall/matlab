db8025ea85a2f774a268e4b2ad70131f27af37fc
Jon Lighthall <jon.lighthall@gmail.com>
2019-11-07 09:29:23 -0600

    inputs and outputs for geod2geoc and geodeticlatitudefromgeocentric working; copy code from rad2geod

--- geoc2geod.m
+++ geoc2geod.m
@@ -1,4 +1,4 @@
-function [glat] = geoc2geod(lat,f,do_match,~)
+function [glat] = geoc2geod(lat,f,do_match,R)
 %GEOC2GEOD Convert geocentric latitude to geodetic latitude. Only valid for
 %points on the surface of the ellipsoid.
 %   Replaces geocentricLatitude from the Mapping Toolbox and geoc2geod from
@@ -9,9 +9,11 @@ do_rad=false;
 switch nargin
     case 4
         % assuming geod2geoc input
-        lat=lat';
-        f=do_match;
-        do_match=true;
+        do_radius=true;
+        model=do_match;
+        E=oblateSpheroid;
+        E.InverseFlattening=1/model;
+        E.SemimajorAxis=R;       
     case 3
         if(ischar(do_match))
             % assuming geocentricLatitude input
@@ -19,33 +21,50 @@ switch nargin
                 do_match=true;
             elseif(strcmp(do_match,'radians')==1)
                 do_match=true;
-                do_rad=true;
+                do_radian=true;
             else
                 % assuming geoc2geod input
-                E=referenceEllipsoid(do_match);
-                f=E.Flattening;
-                fprintf('not valid for non-zero altitudes')
+                do_radius=true;
+                model=do_match;
+                E=referenceEllipsoid(model);
             end
         end
     case 2
         if f>1/5
             % assuming geod2geoc input
+            do_radius=true;
             E=referenceEllipsoid('Earth');
-            f=E.Flattening;
-            fprintf('not valid for non-zero altitudes')
         end
         do_match=true;
+end
+
+if(do_radius)
+    r=f;
+    f=E.Flattening;
+    R=E.SemimajorAxis;
 end
 
 if(f==0)
     glat=lat;
 else
-    if(do_rad)
+    if(do_radian)
         glat=atan2(sin(lat)/((1-f)^2),cos(lat));
-    else
+    else            
         glat=atan2d(sind(lat)/((1-f)^2),cosd(lat));
         if(~do_match)
             glat=mod(glat,360);
+        end
+        if(do_radius)
+            ua=glat;
+            ra=geoc2rad(lat,f,R); % geocentric radius at point on surface intersecting radius
+            l=r-ra; %g  eocentric altitude
+            dlamda=ua-lat;
+            h=l.*cosd(dlamda); %geodetic height
+            rhoa=R.*(1-f).^2./(1-(2.*f-f.^2).*sind(ua).^2).^(3/2);
+            %rhoa=rcurve('meridian',E,ua,'degrees');
+            du=rad2deg(atan2(l.*sind(dlamda),rhoa+h));
+            phi=ua-du;
+            glat=phi;
         end
     end
 end
