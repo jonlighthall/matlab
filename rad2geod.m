96c0cdecb26d4cab74921f0c6f0cbab40941f1c9
Jon Lighthall <jon.lighthall@gmail.com>
2019-11-07 09:48:42 -0600

    unify rad2geod

--- rad2geod.m
+++ rad2geod.m
@@ -1,31 +1,28 @@
-function [phi] = rad2geod(lat,r,mod,R)
+function [phi] = rad2geod(psi,r,model,R)
 %GEOC2GEOD Convert geocentric latitude to geodetic latitude.
-%   Replaces geocentricLatitude from the Mapping Toolbox and geoc2geod from
-%   the Aerospace Toolbox.
+%   Replaces geoc2geod from the Aerospace Toolbox.
 %
-%   See also GEODETICLATITUDEFROMGEOCENTRIC GEOCENTRICLATITUDE GEOD2GEOC
+%   See also GEOC2GEOD GEOD2GEOC GEODETICLATITUDEFROMGEOCENTRIC
+%   GEOCENTRICLATITUDE
 switch nargin
     case 4
         E=oblateSpheroid;
-        E.InverseFlattening=1/mod;
+        E.InverseFlattening=1/model;
         E.SemimajorAxis=R;
-        
     case 3
-        E=referenceEllipsoid(mod);
+        E=referenceEllipsoid(model);
     case 2
         E=referenceEllipsoid('Earth');
-        
 end
 f=E.Flattening;
 R=E.SemimajorAxis;
-ua=geoc2geod(lat,f); % geocentric latitude at point on surface intersecting radius
-ra=geoc2rad(lat,f,R); % geocentric radius at point on surface intersecting radius
+ua=geoc2geod(psi,f); % geocentric latitude at point on surface intersecting radius
+ra=geoc2rad(psi,f,R); % geocentric radius at point on surface intersecting radius
 l=r-ra; %geocentric altitude
-dlamda=ua-lat;
+dlamda=ua-psi;
 h=l.*cosd(dlamda); %geodetic height
 rhoa=R.*(1-f).^2./(1-(2.*f-f.^2).*sind(ua).^2).^(3/2);
-rhoa=rcurve('meridian',E,ua,'degrees');
-du=rad2deg(atan2(l.*sind(dlamda),rhoa+h))
+du=rad2deg(atan2(l.*sind(dlamda),rhoa+h));
 phi=ua-du;
 end
 
