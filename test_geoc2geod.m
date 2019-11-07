77ac87d2054b76061018872ec21ed418aa740c89
Jon Lighthall <jon.lighthall@gmail.com>
2019-11-07 09:49:27 -0600

    add test_geoc2geod, remove rad2geod (redundant)

---
+++ test_geoc2geod.m
@@ -0,0 +1,11 @@
+%test geoc2geod inputs/outputs
+gd = geoc2geod(45, 6379136)
+gd = geoc2geod([0 45 90], 6379136, 'WGS84')
+f = 1/196.877360;
+Re = 3397000;
+gd = geoc2geod([0 45 90], 6379136, f, Re)
+
+% test geodeticLatitudeFromGeocentric inputs/outputs
+s = wgs84Ellipsoid;
+geoc2geod(45, s.Flattening)
+geoc2geod(pi/3, s.Flattening, 'radians')
