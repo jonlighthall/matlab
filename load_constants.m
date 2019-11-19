e1810b4c31be36d5143fc035df526949f4a8b748
Jon Lighthall <jon.lighthall@gmail.com>
2019-11-19 14:13:40 -0600

    add load_constants

---
+++ load_constants.m
@@ -0,0 +1,4 @@
+%% conversion constants
+ft2m=0.3048; %feet to meters (exact)
+nmi2m=1852; %nautical miles to meters (exact)
+db_m2yd=-20*log10(1/(3*ft2m)); %decibels, meter reference to yard reference
