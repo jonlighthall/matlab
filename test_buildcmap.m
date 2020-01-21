61e42f075bf036ba8d788359919f37679ae6dfa4
Jon Lighthall <jon.lighthall@gmail.com>
2020-01-21 11:01:36 -0600

    fix force levels levels are now truncated add trunc levels to test add comments and titles add warnings to buildcmap output

--- test_buildcmap.m
+++ test_buildcmap.m
@@ -35,6 +35,8 @@ colormap(cmap)
 title('tertiary')
 
 %% test levels
+% for each example, the "natural" number of levels is calculated for a
+% given gradient size per color
 levels=2; % gradient size between colors
 
 %grayscale
@@ -45,7 +47,7 @@ n_contour = len+(len-1)*levels;
 [cmap]=buildcmap(colors,n_contour); 
 imshow(im), colorbar
 colormap(cmap) 
-title('grayscale')
+title(sprintf('grayscale, %d levels',n_contour))
 
 % primary
 figure
@@ -55,6 +57,7 @@ n_contour = len+(len-1)*levels;
 [cmap]=buildcmap(colors,n_contour); 
 imshow(im), colorbar
 colormap(cmap) 
+title(sprintf('primary, %d levels',n_contour))
 
 % secondary
 figure
@@ -65,6 +68,7 @@ n_contour = len+(len-1)*levels;
 [cmap]=buildcmap(colors,n_contour); 
 imshow(im), colorbar
 colormap(cmap) 
+title(sprintf('secondary, %d levels',n_contour))
 
 % tertiary
 figure
@@ -74,9 +78,12 @@ n_contour = len+(len-1)*levels;
 [cmap]=buildcmap(colors,n_contour); % add cyan, magenta, black
 imshow(im), colorbar
 colormap(cmap) 
+title(sprintf('tertiary, %d levels',n_contour))
 
 %% test force
-n_contour=14;
+% The number of levels is forced. For color inputs whose length is greater
+% than the number of levels, the colors are truncated.
+n_contour=3;
 
 %grayscale
 figure
@@ -84,7 +91,7 @@ colors='kw'; % names of colors to use
 [cmap]=buildcmap(colors,n_contour,true); 
 imshow(im), colorbar
 colormap(cmap) 
-title('grayscale')
+title(sprintf('grayscale, %d levels forced',n_contour))
 
 % primary
 figure
@@ -92,6 +99,7 @@ colors='bgr'; % number of colors to use
 [cmap]=buildcmap(colors,n_contour,true); 
 imshow(im), colorbar
 colormap(cmap) 
+title(sprintf('primary, %d levels forced',n_contour))
 
 % secondary
 figure
@@ -99,6 +107,7 @@ colors='bcgyrm'; % add cyan, magenta, black
 [cmap]=buildcmap(colors,n_contour,true); 
 imshow(im), colorbar
 colormap(cmap) 
+title(sprintf('secondary, %d levels forced',n_contour))
 
 % tertiary
 figure
@@ -106,5 +115,46 @@ colors='bzcaghyorsmv'; % add cyan, magenta, black
 [cmap]=buildcmap(colors,n_contour,true); % add cyan, magenta, black
 imshow(im), colorbar
 colormap(cmap) 
+title(sprintf('tertiary, %d levels forced',n_contour))
+
+%% test force
+n_contour=14;
+% The number of levels is forced. A number of levels is chosen to conflict
+% with the "natural" number of levels. For color inputs whose calculated
+% gradient is less than the given number of levels, an additional color
+% gradient is added to the top of the colorbar. The added color is the
+% complementary color of the highest colorbar value.
+
+%grayscale
+figure
+colors='kw'; % names of colors to use
+[cmap]=buildcmap(colors,n_contour,true); 
+imshow(im), colorbar
+colormap(cmap) 
+title(sprintf('grayscale, %d levels forced',n_contour))
+
+% primary
+figure
+colors='bgr'; % number of colors to use
+[cmap]=buildcmap(colors,n_contour,true); 
+imshow(im), colorbar
+colormap(cmap) 
+title(sprintf('primary, %d levels forced',n_contour))
+
+% secondary
+figure
+colors='bcgyrm'; % add cyan, magenta, black
+[cmap]=buildcmap(colors,n_contour,true); 
+imshow(im), colorbar
+colormap(cmap) 
+title(sprintf('secondary, %d levels forced',n_contour))
+
+% tertiary
+figure
+colors='bzcaghyorsmv'; % add cyan, magenta, black
+[cmap]=buildcmap(colors,n_contour,true); % add cyan, magenta, black
+imshow(im), colorbar
+colormap(cmap) 
+title(sprintf('tertiary, %d levels forced',n_contour))
 
 autoArrangeFigures
