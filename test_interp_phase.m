acd2a24044095f0f419dc696135f8e708333058f
Jon Lighthall <jon.lighthall@gmail.com>
2020-07-16 11:48:34 -0500

    add display names and legend

--- test_interp_phase.m
+++ test_interp_phase.m
@@ -3,7 +3,7 @@ clear variables
 
 %% truth
 xmin=0;
-xmax=2*pi;
+xmax=2*pi*3;
 x=xmin:.1:xmax;
 x=x.';
 %r=1:(10-1)/(length(x)-1):10;
@@ -13,30 +13,33 @@ z=r.*exp(1i*x);
 %z=z.';
 
 figure
-plot(x/pi,abs(z))
+plot(x/pi,abs(z),'DisplayName','truth: magnitude')
 grid on
+xlabel('phase/pi')
 hold on
-plot(x/pi,real(z))
-plot(x/pi,imag(z))
+plot(x/pi,real(z),'DisplayName','truth: real')
+plot(x/pi,imag(z),'DisplayName','truth: imaginary')
 
 %% sampling
-x1=1:1:floor(xmax);
+x1=1:3:floor(xmax);
 z1=interp1(x,z,x1);
-a1=angle(z1);
-a1b=atan2(imag(z1),imag(z1));
+%a1=angle(z1);
+%a1b=atan2(imag(z1),imag(z1));
 style='x';
-plot(x1/pi,real(z1),style)
-plot(x1/pi,imag(z1),style)
-plot(x1/pi,abs(z1),style)
+name='sample';
+plot(x1/pi,real(z1),style,'DisplayName',name)
+plot(x1/pi,imag(z1),style,'DisplayName',name)
+plot(x1/pi,abs(z1),style,'DisplayName',name)
 
 %% interpolation
 % interp on components
 x2=xmin:.25:xmax;
 z2=interp1(x1,z1,x2);
 style='-o';
-plot(x2/pi,real(z2),style)
-plot(x2/pi,imag(z2),style)
-plot(x2/pi,abs(z2),style)
+name='interp components';
+plot(x2/pi,real(z2),style,'DisplayName',name)
+plot(x2/pi,imag(z2),style,'DisplayName',name)
+plot(x2/pi,abs(z2),style,'DisplayName',name)
 
 % interp on magnitude
 mag=abs(z1);
@@ -45,15 +48,21 @@ m2=interp1(x1,mag,x2);
 a2=interp1(x1,phase,x2);
 z3=m2.*exp(1i*a2);
 style='--+';
-plot(x2/pi,real(z3),style)
-plot(x2/pi,imag(z3),style)
-plot(x2/pi,abs(z3),style)
+name='iterp phase';
+plot(x2/pi,real(z3),style,'DisplayName',name)
+plot(x2/pi,imag(z3),style,'DisplayName',name)
+plot(x2/pi,abs(z3),style,'DisplayName',name)
 
 % interp on phase
 phase2=unwrap(phase);
 a2b=interp1(x1,phase2,x2);
 z3b=m2.*exp(1i*a2b);
 style=':*';
-plot(x2/pi,real(z3b),style)
-plot(x2/pi,imag(z3b),style)
-plot(x2/pi,abs(z3b),style)
+name='unwrap phase';
+plot(x2/pi,real(z3b),style,'DisplayName',name)
+plot(x2/pi,imag(z3b),style,'DisplayName',name)
+plot(x2/pi,abs(z3b),style,'DisplayName',name)
+
+legend('Location','Northwest')
+
+autoArrangeFigures
