36dcc45294b2489c69fef52e685f5f04c5d6935f
Jon Lighthall <jon.lighthall@gmail.com>
2020-09-15 13:20:17 -0500

    add var and comments, Sept 10

--- geo/test_geoc2geod.m
+++ geo/test_geoc2geod.m
@@ -1,9 +1,13 @@
 % test geoc2geod inputs/outputs
-gd = geoc2geod(45, 6379136)
-gd = geoc2geod([0 45 90], 6379136, 'WGS84')
+radius=6379136;
+% test 2 inputs
+gd = geoc2geod(45, radius)
+% test 3 inputs with arrayed lat
+gd = geoc2geod([0 45 90], radius, 'WGS84')
+% test 4 inputs
 f = 1/196.877360;
 Re = 3397000;
-gd = geoc2geod([0 45 90], 6379136, f, Re)
+gd = geoc2geod([0 45 90], radius, f, Re)
 
 % test geodeticLatitudeFromGeocentric inputs/outputs
 s = wgs84Ellipsoid;
