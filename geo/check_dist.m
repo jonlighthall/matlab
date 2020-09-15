9c678310fc82d8aea6da5996cd21caab3ed77024
Jon Lighthall <jon.lighthall@gmail.com>
2020-09-15 13:25:19 -0500

    add handling for default ellipsoid

--- geo/check_dist.m
+++ geo/check_dist.m
@@ -1,11 +1,18 @@
-function check_dist(lat1,lon1,lat2,lon2,elips)
+function check_dist(lat1,lon1,lat2,lon2,ellipoid)
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
-[r_distance,b_distance]=distance(lat1,lon1,lat2,lon2,referenceEllipsoid(elips));
+if (nargin <5)
+    ellipoid='wgs84';
+end
+
+[r_distance,b_distance]=distance(lat1,lon1,lat2,lon2,...
+    referenceEllipsoid(ellipoid));
 
 ln=length(lat1);
+r_dist=zeros(1,ln);
 for i=1:ln
-   [r_dist(i),b_dist,br_dist]=dist([lat1(i) lat2(i)], [lon1(i) lon2(i)],elips);
+   [r_dist(i),b_dist,br_dist]=dist([lat1(i) lat2(i)],[lon1(i) lon2(i)],...
+       ellipoid);
 end
 col=2;
 sz = [ln col];
@@ -13,12 +20,9 @@ varTypes =  strings([col,1])+'double';
 varNames = {'distance';'dist'};
 ranges = table('Size',sz,'VariableTypes',varTypes','VariableNames',varNames);
 
-disp(r_dist')
-disp(r_distance)
-
 ranges.dist=r_dist';
 ranges.distance=r_distance;
 ranges.diff=r_dist'-r_distance;
-ranges
+disp(ranges)
 end
 
