3e97d164a59d43da38f0f3577760a09d9b833e22
Jon Lighthall <jon.lighthall@gmail.com>
2021-07-27 17:43:01 -0500

    add sanitized startup

---
+++ test/startup.m
@@ -0,0 +1,15 @@
+%% paths
+macropath = 'C:\Users\jlighthall\Documents\MATLAB\macros';
+if exist(macropath,'dir')~=7
+  warning('%s not found\n',macropath);
+end
+
+addpath(macropath)
+addpath([macropath '\geo'])
+
+%% macros
+load_constants
+
+%% favorites
+% home cd(userpath);
+% repos cd('X:\forjon\matlab')
