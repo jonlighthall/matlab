731346e570740c1c32f6fae43e4803fd435f991a
Jon Lighthall <jon.lighthall@gmail.com>
2021-09-08 18:25:16 -0500

    add global, miles, mph

--- load_constants.m
+++ load_constants.m
@@ -14,17 +14,27 @@ else
     lc_do_print=false;
 end
 
+license('test','aerospace_toolbox')
+
 %% multiplicitive constants
 % equivalence definitions
+global ft2m
 ft2m=0.3048; % feet to meters (exact)
+global nmi2m
 nmi2m=1852; % nautical miles to meters (exact)
-nmi2km=nmi2m/1000;
+global mi2m
+%mi2m=1609.344; % statute miles to meters (exact)
+mi2m=ft2m*5280; % statute miles to meters
 
 % functional definitions
+% speed
 m2nmi=1/nmi2m; % meters to nautical miles
-kt2ms=nmi2m/(60*60); % knots to meters-per-second
+kt2ms=nmi2m/(60*60); % knots (nautical miles per hour) to meters-per-second
 ms2kt=1/kt2ms; % meters-per-second to knots
+kt2mph=nmi2m/mi2m; % knots to miles-per-hour
 
+% distance
+nmi2km=nmi2m/1000; % nautical miles to kilometers
 m2ft=1/ft2m; % meters to feet
 m2yd=m2ft/3; % meters to yards
 yd2m=1/m2yd; % yards to meters
@@ -47,6 +57,8 @@ if(lc_do_print)
     lc_fmt=sprintf(' %%%d.%df',lc_dp+3,lc_dp); % floating point
    %fmt=sprintf(' %%%d.%de',dp+6,dp-1); % scientific
     fprintf(strcat('  nmi2m = ',lc_fmt,'\n'),nmi2m);
+    fprintf(strcat('   mi2m = ',lc_fmt,'\n'),mi2m);
+    fprintf('\n');
     fprintf(strcat('  m2nmi = ',lc_fmt,'\n'),m2nmi);
     fprintf(strcat('  kt2ms = ',lc_fmt,'\n'),kt2ms);
     fprintf(strcat('  ms2kt = ',lc_fmt,'\n'),ms2kt);
@@ -56,6 +68,7 @@ if(lc_do_print)
     fprintf(strcat('   m2yd = ',lc_fmt,'\n'),m2yd);
     fprintf(strcat('   yd2m = ',lc_fmt,'\n'),yd2m);
     fprintf(strcat('  kyd2m = ',lc_fmt,'\n'),kyd2m);
+    fprintf('\n');
     fprintf(strcat('dB_m2yd = ',lc_fmt,'\n'),dB_m2yd);
     fprintf(strcat('dB_yd2m = ',lc_fmt,'\n'),dB_yd2m);
 end
