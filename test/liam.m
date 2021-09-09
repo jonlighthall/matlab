7e25c1e165a5876520fd67d76a9c553abb3bc65d
Jon Lighthall <jon.lighthall@gmail.com>
2021-09-09 08:07:19 -0500

    clean up

--- test/liam.m
+++ test/liam.m
@@ -2,13 +2,9 @@ NET.addAssembly('System.Speech');
 obj = System.Speech.Synthesis.SpeechSynthesizer;
 obj.Volume = 100;
 
-% inputText ='fart';
+inputText ='fart';
 
 for i=1:5
-    inputText ='fart';
-    
     fprintf('%d %s\n',i,inputText);
     Speak(obj, inputText);
-    
-    
 end
