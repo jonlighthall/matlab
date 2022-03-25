68279c75870021074884e70ec834b4a346ab627f
Jon Lighthall <jon.lighthall@gmail.com>
2022-03-25 13:16:58 -0500

    add docpath

--- test/startup.m
+++ test/startup.m
@@ -1,14 +1,31 @@
 %% paths
-macropath = 'C:\Users\jlighthall\Documents\MATLAB\macros';
-if exist(macropath,'dir')~=7
-  warning('%s not found\n',macropath);
+macropath = 'C:\Users\jlighthall\Documents';
+fprintf('  docpath: %s is... ',docpath)
+if exist(docpath,'dir')~=7
+    warning('not found\n');
+    return
+else
+    fprintf('found\n')
+    macropath = [docpath '\MATLAB\macros'];
+    fprintf('macropath: %s is... ',macropath)
+    if exist(macropath,'dir')~=7
+        warning('not found\n');
+        return
+    else
+        fprintf('found\n')
+        %% paths
+        addpath(macropath)
+        addpath([macropath '\geo'])
+	addpath([macropath '\math'])
+        
+        %% macros
+        load_constants
+    end
 end
 
-addpath(macropath)
-addpath([macropath '\geo'])
-
-%% macros
-load_constants
+%% settings
+matlab.internal.licensing.enableBorrowUI(true)
+beep off
 
 %% favorites
 % home cd(userpath);
