87a239bea41c9730b495bf76e4794f34e8e33946
Jon Lighthall <jonathan.lighthall@nrlssc.navy.mil>
2023-04-01 13:54:34 -0500

    add stat as return value; Feb 14

--- test_dir.m
+++ test_dir.m
@@ -1,6 +1,7 @@
-function test_dir(dir_in)
+function [stat]=test_dir(dir_in)
 fprintf('%9s: %s is... ',inputname(1),dir_in)
-if exist(dir_in,'dir')~=7
+stat=exist(dir_in,'dir');
+if stat~=7
     fprintf('not found\n ');
     warning('''%s'' not found',dir_in);
     return
