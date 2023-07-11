4ca6141dbb0ca436588d831e0008288b226b00da
Jon Lighthall <jon.lighthall@gmail.com>
2023-07-11 13:22:34 -0500

    add handling for array input for set plot size

--- figs/setplotsize.m
+++ figs/setplotsize.m
@@ -1,13 +1,27 @@
 function setplotsize(scx,scy,sca)
-% set plot size
+% SETPLOTSIZE Set plot size.
+%   Specify the plot dimensions in inches as (x,y,scale). The default
+%   arguments are (10,7.5,1), corresponding to a plot 10" wide and 7.5"
+%   tall.
 switch nargin
     case 0
         scx=10;
         scy=7.5;
         sca=1;
-    case 1
-        scy=scx;
-        sca=1;
+    case 1        
+        switch length(scx)
+            case 1
+                scy=scx;
+                sca=1;
+            case 2
+                scy=scx(2);
+                scx=scx(1);
+                sca=1;
+            case 3
+                sca=scx(3);
+                scy=scx(2);
+                scx=scx(1);
+        end
     case 2
         sca=1;
     otherwise
