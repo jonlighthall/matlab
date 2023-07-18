4d0f087b4160eca9a311d61eb1caba270fbaab5b
Jon Lighthall <jon.lighthall@gmail.com>
2023-07-18 15:50:24 -0500

    add tlus func

--- tl/rand_comp_test.m
+++ tl/rand_comp_test.m
@@ -5,27 +5,26 @@ rng('default');
 nr=256;
 nz=768;
 
+% normalized
 A=rand(nr,nz);
 B=rand(nr,nz);
-%B=A.*1.001;
 
 a=ssim(A,B);
 b=immse(A,B);
 fprintf('Before: %f, %f\n',a,b)
 
-smin = 50;
-sspan= 70;
-smax = smin +sspan;
-
-sA=sspan.*A+smin;
-sB=sspan.*B+smin;
-
-sc=2;
-sa=0;
-sA=A.*sc+sa;
-sB=B.*sc+sa;
+% scaled or un-normalized
+sA=tlus(A);
+sB=tlus(B);
 
 aa=ssim(sA,sB);
 bb=immse(sA,sB);
 
 fprintf(' After: %f, %f\n',aa,bb)
+
+% linear
+clear sA sB
+sc=2;
+sa=0;
+sA=A.*sc+sa;
+sB=B.*sc+sa;
