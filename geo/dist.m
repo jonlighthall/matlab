bf9c0106a569249a5c45ce6f84ab0851daa51d65
Jon Lighthall <jon.lighthall@gmail.com>
2025-02-04 11:37:19 -0600

    define eccentricity squared and add comments

--- geo/dist.m
+++ geo/dist.m
@@ -1,30 +1,30 @@
 function [range,A12,A21]=dist(lat,long,arg1,arg2)
-%DIST Computes distance and bearing between points on the earth using
-% various reference spheroids. 
+% DIST Computes distance and bearing between points on the earth using
+% various reference spheroids.
 %
 %   [RANGE,AF,AR]=DIST(LAT,LONG) computes the ranges RANGE between points
 %   specified in the LAT and LONG vectors (decimal degrees with positive
 %   indicating north/east). Forward and reverse bearings (degrees) are
-%   returned in AF, AR. 
-%   
+%   returned in AF, AR.
+%
 %   [RANGE,GLAT,GLONG]=DIST(LAT,LONG,N) computes N-point geodesics between
 %   successive points. Each successive geodesic occupies it's own row
 %   (N>=2)
-%   
+%
 %   [..]=DIST(...,'ellipsoid') uses the specified ellipsoid to get
 %   distances and bearing. Available ellipsoids are:
-%   
+%
 %   'clarke66'  Clarke 1866
 %   'iau73'     IAU 1973
 %   'wgs84'     WGS 1984
 %   'sphere'    Sphere of radius 6371.0 km
-%   
+%
 %   The default is 'wgs84'.
-%   
+%
 %   Ellipsoid formulas are recommended for distance d<2000 km, but can be
-%   used for longer distances. 
-%   
-%   Notes:    
+%   used for longer distances.
+%
+%   Notes:
 %   A Newhall (WHOI) Sep 1997
 %       Modified and fixed a bug found in Matlab version 5. This routine
 %       may interfere with dist that is supplied with matlab's neural net
@@ -36,27 +36,27 @@ function [range,A12,A21]=dist(lat,long,arg1,arg2)
 %       Feeping Creaturism! - added geodesic computations. This turned out
 %       to be pretty hairy since there were a lot of branch problems with
 %       asin, atan when computing geodesics subtending > 90 degrees that
-%       were ignored in the original code! 
+%       were ignored in the original code!
 %   R. Pawlowicz (WHOI) 15/Jan/91
 %       Fixed some bothersome special cases, like when computing geodesics
 %       and N=2, or LAT=0...
-%   
+%
 %   GIVEN THE LATITUDES AND LONGITUDES (IN DEG.) IT ASSUMES THE IAU SPHERO
 %   DEFINED IN THE NOTES ON PAGE 523 OF THE EXPLANATORY SUPPLEMENT TO THE
-%   AMERICAN EPHEMERIS. 
-%   
+%   AMERICAN EPHEMERIS.
+%
 %   THIS PROGRAM COMPUTES THE DISTANCE ALONG THE NORMAL SECTION (IN M.) OF
 %   A SPECIFIED REFERENCE SPHEROID GIVEN THE GEODETIC LATITUDES AND
 %   LONGITUDES OF THE END POINTS *** IN DECIMAL DEGREES ***
-%   
+%
 %   IT USES ROBBIN'S FORMULA, AS GIVEN BY BOMFORD, GEODESY, FOURTH EDITION,
 %   P. 122.  CORRECT TO ONE PART IN 10**8 AT 1600 KM.  ERRORS OF 20 M AT
-%   5000 KM. 
-%   
+%   5000 KM.
+%
 %   CHECK:  SMITHSONIAN METEOROLOGICAL TABLES, PP. 483 AND 484, GIVES
 %   LENGTHS OF ONE DEGREE OF LATITUDE AND LONGITUDE AS A FUNCTION OF
-%   LATITUDE. (SO DOES THE EPHEMERIS ABOVE) 
-%   
+%   LATITUDE. (SO DOES THE EPHEMERIS ABOVE)
+%
 %   PETER WORCESTER, AS TOLD TO BRUCE CORNUELLE...1983 MAY 27
 
 spheroid='wgs84';
@@ -77,6 +77,9 @@ if (nargin >= 3)
     end
 end
 
+% define spheroid parameters
+%  A (semi-major axis),
+%  B (semi-minor axis)
 if strcmp(spheroid(1:3),'sph')
     A = 6371000.7900;
     B = A;
@@ -87,18 +90,21 @@ elseif strcmp(spheroid(1:3),'iau')
     A = 6378160.;
     B = 6356774.516;
 elseif strcmp(spheroid(1:3),'wgs')
-    % on 9/11/88, Peter Worcester gave me the constants for the  WGS84
-    % spheroid, and he gave A (semi-major axis), F = (A-B)/A (flattening)
-    % (where B is the semi-minor axis)
+    % on 9/11/88, Peter Worcester gave me the constants for the WGS84
+    % spheroid
     A = 6378137.;
+	%  F = (A-B)/A (flattening)
     F = 1./298.257223563;
     B = A*(1-F);
 else
     error('dist: Unknown spheroid specified!');
 end
-E = sqrt(A*A-B*B)/A;
-EPS= E*E/(1-E*E);
+% calculate first eccentricity squared (ee squared)
+ES=1-(B^2)/(A^2);
+% calculate second eccentricity squared (ee prime squared)
+EPS= ES/(1-ES);
 
+% check coordinate array sizes
 NN=max(size(lat));
 if (NN ~= max(size(long)))
     error('dist: Lat, Long vectors of different sizes!');
@@ -135,13 +141,13 @@ if (any(XLAM1==XLAM2))
 end
 
 % COMPUTE THE RADIUS OF CURVATURE IN THE PRIME VERTICAL FOR EACH POINT
-xnu=A./sqrt(1.0-(E*sin(lat)).^2);
+xnu=A./sqrt(1.0-ES*sin(lat).^2);
 xnu1=xnu(1:NN-1);
 xnu2=xnu(2:NN);
 
 % COMPUTE THE AZIMUTHS.  A12 (A21) IS THE AZIMUTH AT POINT 1 (2) OF THE
 % NORMAL SECTION CONTAINING THE POINT 2 (1)
-TPSI2=(1.-E*E)*tan(PHI2) + E*E*xnu1.*sin(PHI1)./(xnu2.*cos(PHI2));
+TPSI2=(1.-ES)*tan(PHI2) + ES*xnu1.*sin(PHI1)./(xnu2.*cos(PHI2));
 PSI2=atan(TPSI2);
 
 % SOME FORM OF ANGLE DIFFERENCE COMPUTED HERE??
@@ -191,17 +197,17 @@ TERM4=-(SIG.^5).*G.*H/48.0;
 range=xnu1.*SIG.*(1.0+TERM1+TERM2+TERM3+TERM4);
 
 if (geodes)
-    
+
     % now calculate the locations along the ray path. (for extra accuracy,
     % could do it from start to halfway, then from end for the rest,
     % switching from A12 to A21... started to use Rudoe's formula, page 117
     % in Bomford...(1980, fourth edition) but then went to Clarke's best
     % formula (pg 118)
-    
+
     % RP I am doing this twice because this formula doesn't work when we go
     % past 90 degrees!
     Ngd1=round(Ngeodes/2);
-    
+
     % First time...away from point 1
     if (Ngd1>1)
         wns=ones(1,Ngd1);
@@ -212,10 +218,10 @@ if (geodes)
         C2 = R3PRM.*(1.0+3.0*R2PRM)/24.0*wns;
         R2PRM=R2PRM*wns;
         R3PRM=R3PRM*wns;
-        
+
         % now have to loop over positions
         RLRAT = (range./xnu1)*([0:Ngd1-1]/(Ngeodes-1));
-        
+
         THETA = RLRAT.*(1 - (RLRAT.^2).*(C1 - C2.*RLRAT));
         C3 = 1.0 - (R2PRM.*(THETA.^2))/2.0 - (R3PRM.*(THETA.^3))/6.0;
         DSINPSI =(sin(PHI1)*wns).*cos(THETA) + ...
@@ -223,10 +229,10 @@ if (geodes)
         % try to identify the branch...got to other branch if range> 1/4
         % circle
         PSI = asin(DSINPSI);
-        
+
         DCOSPSI = cos(PSI);
         DSINDLA = (sin(A12)*wns).*sin(THETA)./DCOSPSI;
-        DTANPHI=(1.0+EPS)*(1.0 - (E^2)*C3.*(sin(PHI1)*wns)./DSINPSI).*tan(PSI);
+        DTANPHI=(1.0+EPS)*(1.0 - (ES)*C3.*(sin(PHI1)*wns)./DSINPSI).*tan(PSI);
         % compute output latitude (phi) and long (xla) in radians
         % I believe these are absolute, and don't need source coords added
         PHI = atan(DTANPHI);
@@ -238,7 +244,7 @@ if (geodes)
         PHI=PHI1;
         XLA=XLAM1;
     end
-    
+
     % Now we do the same thing, but in the reverse direction from the
     % receiver!
     if (Ngeodes-Ngd1>1)
@@ -250,10 +256,10 @@ if (geodes)
         C2 = R3PRM.*(1.0+3.0*R2PRM)/24.0*wns;
         R2PRM=R2PRM*wns;
         R3PRM=R3PRM*wns;
-        
+
         % now have to loop over positions
         RLRAT = (range./xnu2)*([0:Ngeodes-Ngd1-1]/(Ngeodes-1));
-        
+
         THETA = RLRAT.*(1 - (RLRAT.^2).*(C1 - C2.*RLRAT));
         C3 = 1.0 - (R2PRM.*(THETA.^2))/2.0 - (R3PRM.*(THETA.^3))/6.0;
         DSINPSI =(sin(PHI2)*wns).*cos(THETA) + ...
@@ -261,10 +267,10 @@ if (geodes)
         % try to identify the branch...got to other branch if range> 1/4
         % circle
         PSI = asin(DSINPSI);
-        
+
         DCOSPSI = cos(PSI);
         DSINDLA = (sin(A21)*wns).*sin(THETA)./DCOSPSI;
-        DTANPHI=(1.0+EPS)*(1.0 - (E^2)*C3.*(sin(PHI2)*wns)./DSINPSI).*tan(PSI);
+        DTANPHI=(1.0+EPS)*(1.0 - (ES)*C3.*(sin(PHI2)*wns)./DSINPSI).*tan(PSI);
         % compute output latitude (phi) and long (xla) in radians
         % I believe these are absolute, and don't need source coords added
         PHI = [PHI fliplr(atan(DTANPHI))];
@@ -276,17 +282,17 @@ if (geodes)
         PHI = [PHI PHI2];
         XLA = [XLA XLAM2];
     end
-    
+
     % convert to degrees
     A12 = PHI*180/pi;
     A21 = XLA*180/pi;
     range=range*([0:Ngeodes-1]/(Ngeodes-1));
 else
-    
+
     % CONVERT TO DECIMAL DEGREES
     A12=A12*180/pi;
     A21=A21*180/pi;
-   
+
     % It is easier if things are column vectors, but we have to fix things
     % before returning!
     if isrow(lat)
