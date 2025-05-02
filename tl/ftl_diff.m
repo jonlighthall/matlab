8d2cbd0f7022d8eca91375ad09e27ebf036c2498
Jon Lighthall <jon.lighthall@gmail.com>
2025-05-02 14:16:35 -0500

    tl diff: add print stat and update comments

--- tl/ftl_diff.m
+++ tl/ftl_diff.m
@@ -1,10 +1,8 @@
-function [tl_diff_1] = ftl_diff(tl1,tl2)
-%fTL_diff Trasnmission Loss Weighted Differnce.
-%   Calculates the weighting factor for a given transmission loss based on
-%   the formulation presented in the following reference. The function
-%   parameters are hard-coded based on the values from the given reference.
-%
-%   The weighted mean absolute error is returm
+function [tl_diff_1] = ftl_diff(tl1,tl2,print_stat)
+%ftl_diff - Weighted Trasnmission Loss Differnce
+%   Calculates the weighted Mean Absolute Error between two transmission
+%   loss curves based on the formulation presented in the following
+%   reference. Returns the quantity "TL_1^diff" from reference.
 %
 %   https://doi.org/10.23919/OCEANS.2009.5422312
 %
@@ -22,12 +20,19 @@ weighted_diff=tl_diff.*tl_weight;
 % calculate weighted mean absolute difference
 tl_diff_1=sum(weighted_diff)/sum_weight;
 
-ln=length(tl1);
-% fprintf('number of elements = %d \n',ln)
-% fprintf('    sum of weights = %.1f \n',sum_weight)
-% fprintf('average weight = %f \n',sum_weight/ln)
-% 
-% fprintf('unweighted MAE = %f\n',mean(tl_diff) )
-% fprintf('  weighted MAE = %f\n',tl_diff_1)
+% print statistics
+if nargin < 3
+    % set default value
+    print_stat=false;
+end
 
+if print_stat
+    ln=length(tl1);
+    fprintf('number of elements = %d \n',ln)
+    fprintf('    sum of weights = %.1f \n',sum_weight)
+    fprintf('average weight = %f \n',sum_weight/ln)
+
+    fprintf('unweighted MAE = %f\n',mean(tl_diff) )
+    fprintf('  weighted MAE = %f\n',tl_diff_1)
+end
 end
