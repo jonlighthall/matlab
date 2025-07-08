dad55fe1b6645ca767117d166c587cf877234551
Jon Lighthall <jon.lighthall@gmail.com>
2025-07-08 16:40:51 -0500

    make sun ang a function, add 2018 sol rad, add nominal solar radius, make var names more descriptive, add arcseconds

--- sun_angle.m
+++ sun_angle.m
@@ -1,38 +1,64 @@
-close all
-clear variables 
+function solar_dia_deg = sun_angle(distance_from_sun, solar_radius)
+%SUN_ANGLE Calculate the angular size of the sun from a given distance.
+%   ANG = SUN_ANGLE(DISTANCE_FROM_SUN) returns the angular size in degrees
+%   of the sun as seen from DISTANCE_FROM_SUN (in meters).
+%   If DISTANCE_FROM_SUN is not provided, the astronomical unit is used.
 
 % astronomical unit in m
 au=149597870700;
 
-% solar radius in m
-solrad=6.95700e8;
+if nargin < 1
+    % astronomical unit in m
+    distance_from_sun = 1.0 * au; % default to 1 AU
+end
+
+% International Astronomical Union (IAU)
+% nominal solar radius in m (R_sol^N)
+% DOI 10.3847/0004-6256/152/2/41
+nominal_solar_radius=6.95700e8;
 
 % measured solar radius from Mercury transits
 % https://arxiv.org/abs/1203.4898
 % 696,342 ± 65km
-sunrad=696342e3; 
+solar_radius_2011=696342e3;
 
-% compare to definition
-fprintf('One solar radius is %.8f au\n',solrad/au)
-fprintf('The sun''s radius is %.8f au\n',sunrad/au)
-fprintf('The sun''s radius is %.8f RN\n',sunrad/solrad)
+% arXiv:1605.09788
+solar_radius_2018=695823e3;
+
+if nargin < 2
+    % use the nominal solar radius if not provided
+    solar_radius = solar_radius_2018;
+end
 
 % solar diameter in m
-soldia=2*solrad;
+solar_dia_meter=2*solar_radius;
 
-% angular szie in radians
-del=2*atan(soldia/(2*au));
-
-% angular size in degrees
-ang=rad2deg(del);
+% angular size in radians
+solar_dia_rad=atan(solar_dia_meter/(distance_from_sun));
 
 % angular size in degrees
-arcmin=ang*60; % should be between ~31.5 32.5 arcminutes
+solar_dia_deg=rad2deg(solar_dia_rad);
 
-fprintf('The angular size of the sun is %.4f%s or %.2f''\n',ang,char(176),arcmin)
-
-%arcsec=mod(arcmin,1)*60
+% angular size in degrees
+solar_dia_arcmin=solar_dia_deg*60; % should be between ~31.5 32.5 arcminutes
 
 % angular radius in degrees
-angrad=ang/2 % should be about 1/4 deg
+solar_rad_deg=solar_dia_deg/2; % should be about 1/4 deg
 
+% solar radius in arcseconds
+solar_rad_arcsec=solar_rad_deg*3600; % should be about 960 arcseconds
+
+if nargout > 0
+    % return the angular size in degrees
+    return
+end
+% if no output argument, print the results
+
+
+fprintf('INPUTS:\n')
+fprintf('  Distance from the sun: %.5g au\n',distance_from_sun/au)
+fprintf('  Solar radius: %.5g nominal solar radii\n',solar_radius/nominal_solar_radius)
+
+fprintf('OUTPUTS:\n')
+fprintf('  Angular size: %.5f%s or %.5g'' or %.5g''''\n',solar_dia_deg,char(176),solar_dia_arcmin,solar_dia_arcmin*60)
+fprintf('  Solar radius: %.5g%s or %.5g'' or %.5g''''\n',solar_rad_deg,char(176),solar_rad_arcsec/60,solar_rad_arcsec)
